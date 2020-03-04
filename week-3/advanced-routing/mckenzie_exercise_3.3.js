75d892b912e1472b67deeaff4694d0ffbeb93fbc
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-03-04 14:51:06 -0500

    adding

---
+++ week-3/advanced-routing/mckenzie_exercise_3.3.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title:  advanced-routing.js
+; Author: Professor Krasso
+; Date:   4 March 2020
+; Modified By: Diandra McKenzie
+; Description: Demonstrates advanced routing
+;===========================================
+*/
+
+//require statements for required libraries
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+//set the views
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+
+app.get('/:employeeId', function(request, response) {
+    
+    var employeeId = parseInt(request.params.employeeId, 10);
+
+    response.render('index', {
+        employeeId: employeeId
+    });
+});
+
+//port listening and logging
+
+http.createServer(app).listen(3001, function() {
+    console.log('Application started and is listening on port %s', 3001);
+});
+
+//end program
