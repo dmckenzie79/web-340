368e4ef33c615c79df61b7c78db7f4cb25b2e466
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-03-31 16:38:31 -0400

    update

--- week-7/tdd-example/test/mckenzie_exercise_7.1.js
+++ week-7/tdd-example/test/mckenzie_exercise_7.1.js
@@ -10,8 +10,10 @@
 
 //start program
 
+//require statement
 var assert = require("assert");
 
+//test
 describe("String#split", function() {
     it("should return an array of fruits", function() {
         assert(Array.isArray("Apple,Orange,Mango".split(",")));
