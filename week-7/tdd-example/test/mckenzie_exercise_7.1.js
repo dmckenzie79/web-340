0003454d7caba14eec4d49864e3a11a883eedae1
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-03-31 15:44:19 -0400

    adding

---
+++ week-7/tdd-example/test/mckenzie_exercise_7.1.js
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title:  fruit.js
+; Author: Professor Krasso
+; Date:   31 March 2020
+; Modified By: Diandra McKenzie
+; Description: Demonstrations how to create a TDD unit test.
+;===========================================
+*/
+
+//start program
+
+var assert = require("assert");
+
+describe("String#split", function() {
+    it("should return an array of fruits", function() {
+        assert(Array.isArray("Apple,Orange,Mango".split(",")));
+    });
+});
+
+
+
+function getFruits(str) {
+
+    return str.split(',');
+
+   }
+
+   module.exports = getFruits;
+
+//end program
