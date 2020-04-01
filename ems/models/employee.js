0033810b4391a62b9cf1b85be5346357a6509b37
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-03-31 21:36:28 -0400

    adding

---
+++ ems/models/employee.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title:  employee.js
+; Author: Professor Krasso
+; Date:   31 March 2020
+; Modified By: Diandra McKenzie
+; Description: File for the Employee database model
+;===========================================
+*/
+
+//start program
+
+// Require statements
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// define Employee Schema
+var employeeSchema = new Schema({
+  firstName: {type: String, required: true },
+  lastName: {type: String, required: true }
+});
+
+// define Employee Model
+var Employee = mongoose.model("Employee", employeeSchema);
+   
+// Export the model so its publicly available.
+module.exports = Employee;
+
+//end program
