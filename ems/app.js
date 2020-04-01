f52ae783840fa89e9ffd82beba574bd7f6e77c2b
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-03-31 21:37:36 -0400

    update

--- ems/app.js
+++ ems/app.js
@@ -15,6 +15,25 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+var Employee = require("./models/employee.js");
+
+// MongoDB compass connection
+var mongoDB = 'mongodb+srv://dmck1979:Kirk1976@buwebdev-cluster-1-42fge.mongodb.net/test';
+
+//create connection
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.Promise - global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function() {
+    console.log("Application connected to MongoDB instance");
+
+});
 
 //app functions
 var app = express();
@@ -28,6 +47,12 @@ app.get('/', function(req, res) {
     res.render('index', {
         title: "Homepage"
     });
+});
+
+//model
+var Employee = new Employee({
+    firstName: "John",
+    lastName: "Smith"
 });
 
 //create server
